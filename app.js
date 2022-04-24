b90961c5e539106d683c10bee7573ef70d0af171
Thomas Ciszek <tciszek515@gmail.com>
2022-04-24 17:45:41 -0400

    finished codebase

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generatePage = require("./src/page-template");
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 
 
@@ -134,18 +135,22 @@ const promptProject = portfolioData => {
   });
 };
 
-
-
-promptUser()
+  promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    const pageHTML = generatePage(portfolioData)
-
-    fs.writeFile('index.html', pageHTML, err => {
-      err ? new Error(err) : console.log('Portfolio complete! Check out index.html to see the output!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-
-
 
